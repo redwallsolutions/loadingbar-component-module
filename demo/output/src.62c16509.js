c7876882c5ad2aad770fccf29a2a4bdf263178c4
Andrew Ribeiro <andtankian@gmail.com>
2019-11-04 12:19:01 -0300

    Fixed, removed --open option to parcel build script

--- demo/output/src.62c16509.js
+++ demo/output/src.62c16509.js
@@ -38608,7 +38608,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44371" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34579" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
