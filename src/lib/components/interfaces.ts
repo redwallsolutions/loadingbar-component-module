c9357f0ce8513ac823102649ad057885de3ec91a
Andrew Ribeiro <andtankian@gmail.com>
2019-11-17 18:39:15 -0300

    Improved, updated some packages and improved docz

--- src/lib/components/interfaces.ts
+++ src/lib/components/interfaces.ts
@@ -6,7 +6,9 @@ export interface ILoadingBar extends ICommonProps {
 	 */
 	progress?: number
 	/**
-	 * The onFinish callback is a function that is called just after the loadingbar ending animation.
+	 * The onFinish callback is a function that is called just after the loadingbar reaches 100.
+	 * It's important to notice that if you try reset the loading bar inside that callback function
+	 * you should `await` the `finished` parameter.
 	 */
 	onFinish?: (finished:Promise<void>)=>void
 }
